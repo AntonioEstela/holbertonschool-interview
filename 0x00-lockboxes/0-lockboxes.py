82cee7e27e349ab2dbfec6d6ca8cdfb10a09e82d
Antonio Estela <antonioestela73@gmail.com>
2020-12-10 13:39:51 -0500

    Added task 0

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Method that determines if all the boxes can be opened."""
+
+
+def canUnlockAll(boxes):
+    """Method that determines if all the boxes can be opened."""
+
+    keys = [0]
+    indxBox = 0
+
+    if len(boxes) == 0:
+        return True
+
+    for box in boxes:
+        if indxBox in keys:
+           for key in box:
+               if key not in keys:
+                   keys.append(key)
+
+        indxBox += 1
+
+    indxBox = 0
+
+    for box in boxes:
+        if indxBox in keys:
+           for key in box:
+               if key not in keys:
+                   keys.append(key)
+
+        indxBox += 1
+
+    return len(keys) == len(boxes)   
