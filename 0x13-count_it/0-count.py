cc8daf4c5909e0750d6d353afca88886b0a31b99
Antonio Estela <40125100+AntonioEstela@users.noreply.github.com>
2021-05-10 00:05:29 +0000

    Restructured code

--- 0x13-count_it/0-count.py
+++ 0x13-count_it/0-count.py
@@ -7,55 +7,39 @@ and prints a sorted count of given keywords
 import requests
 
 
-def recursive_search(subreddit, word_list, dict, after=""):
-    """
-    Recursive function that parses the title of all hot articles,
-    and prints a sorted count of given keywords
-    """
-    url = 'https://api.reddit.com/r/{}/hot?after={}'.format(subreddit, after)
-
-    r = requests.get(
-        url, headers={'User-agent': 'product'}, allow_redirects=False)
-
-    if r.status_code != 200:
-        return None
-
-    if after is None:
-        return dict
-
-    actual = r.json().get('data').get('children')
-    after = r.json().get('data').get('after')
-
-    for post in actual:
-        title = post.get('data').get('title').split()
-
-        for word in set(word_list):
-            word = word.lower()
-            if word in [w.lower() for w in title]:
-                if dict.get(word):
-                    dict[word] += 1
-                else:
-                    dict[word] = 1
-
-    recursive_search(subreddit, word_list, dict, after)
-
-    return dict
-
-
-def print_alphabetically(dict):
-    """
-    Function that prints alphabetically a dictionary
-    """
-    for k, v in sorted(dict.items(), key=lambda value: (-value[1], value[0])):
-        if v != 0:
-            print('{}: {}'.format(k, v))
-
-
-def count_words(subreddit, word_list):
+def count_words(subreddit, word_list, after='', dict={}):
     """
     Recursive function
     """
-    results = recursive_search(subreddit, word_list, {})
+    url = 'https://www.reddit.com/r/{}/hot.json?after={}'
+    r = requests.get(url.format(subreddit, after),
+                      headers={'User-Agent': 'custom'},
+                      allow_redirects=False)
 
-    if results:
-        print_alphabetically(results)
+    if r and r.status_code == 200:
+        list_req = r.json().get('data').get('children')
+
+        for children in list_req:
+            title = children.get('data').get('title')
+
+            for word in word_list:
+                try:
+                    dict[word] += title.lower().split().count(word.lower())
+
+                except KeyError:
+                    dict[word] = title.lower().split().count(word.lower())
+
+        after = r.json().get('data').get('after')
+
+        if (after is None):
+            for key, val in sorted(dict.items(),
+                                   key=lambda x: (-x[1], x[0])):
+                if (val != 0):
+                    print("{}: {}".format(key.lower(), val))
+
+            return
+
+        return count_words(subreddit, word_list, after, dict)
+
+    else:
+        return
